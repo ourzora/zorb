1c1db2faab9297a8bb6fe2987fcaf30644af6ad7
Iain <iain@zora.co>
2021-12-30 21:58:26 -0500

    update deployments

--- packages/zorb-contracts/deploy/02_zorb_nft.ts
+++ packages/zorb-contracts/deploy/02_zorb_nft.ts
@@ -1,22 +1,17 @@
 module.exports = async ({ getNamedAccounts, deployments }: any) => {
   const { deploy } = deployments;
-  const { deployer, erc721base, sharedNFTLogic } = await getNamedAccounts();
-
-  let baseAddress = erc721base;
-  // Deploy in testnet or when no base is deployed
-  if (!baseAddress) {
-    baseAddress = (await deployments.get("BaseTestContract")).address;
-  }
+  const { deployer, sharedNFTLogic } = await getNamedAccounts();
 
   let sharedNFTLogicAddress = sharedNFTLogic;
   // Deploy in testnet or when no base is deployed
   if (!sharedNFTLogic) {
-    sharedNFTLogicAddress = (await deployments.get("TestSharedNFTLogic")).address;
+    sharedNFTLogicAddress = (await deployments.get("TestSharedNFTLogic"))
+      .address;
   }
 
   await deploy("ZorbNFT", {
     from: deployer,
-    args: [baseAddress, sharedNFTLogicAddress],
+    args: [sharedNFTLogicAddress],
     log: true,
   });
 };
