8b33e5b52250ad8e6e3e006794ce24eab7b556d3
Iain <iain@zora.co>
2022-01-05 02:29:51 -0500

    deploying zorb fridge

--- packages/zorb-fridge-contracts/deploy/01_test_shared_nft_logic.ts
+++ packages/zorb-fridge-contracts/deploy/01_test_shared_nft_logic.ts
@@ -1,13 +1,13 @@
 module.exports = async ({ getNamedAccounts, deployments }: any) => {
-	const { deploy } = deployments;
-	const { deployer, sharedNFTLogic } = await getNamedAccounts();
+  const { deploy } = deployments;
+  const { deployer, sharedNFTLogic } = await getNamedAccounts();
 
-	if (!sharedNFTLogic) {
-		await deploy("TestSharedNFTLogic", {
-			from: deployer,
-			args: [],
-			log: true,
-		});
-	}
+  if (!sharedNFTLogic) {
+    await deploy("TestSharedNFTLogic", {
+      from: deployer,
+      args: [],
+      log: true,
+    });
+  }
 };
 module.exports.tags = ["TestSharedNFTLogic"];
--- packages/zorb-fridge-contracts/deploy/02_zorb_nft.ts
+++ packages/zorb-fridge-contracts/deploy/02_zorb_nft.ts
@@ -1,19 +1,21 @@
 module.exports = async ({ getNamedAccounts, deployments }: any) => {
-	const { deploy } = deployments;
-	const { deployer, sharedNFTLogic } = await getNamedAccounts();
+  const { deploy } = deployments;
+  const { deployer, sharedNFTLogic, zorbNFT } = await getNamedAccounts();
 
-	let sharedNFTLogicAddress = sharedNFTLogic;
-	// Deploy in testnet or when no base is deployed
-	if (!sharedNFTLogic) {
-		sharedNFTLogicAddress = (await deployments.get("TestSharedNFTLogic"))
-			.address;
-	}
+  let sharedNFTLogicAddress = sharedNFTLogic;
+  // Deploy in testnet or when no base is deployed
+  if (!sharedNFTLogic) {
+    sharedNFTLogicAddress = (await deployments.get("TestSharedNFTLogic"))
+      .address;
+  }
 
-	await deploy("ZorbNFT", {
-		from: deployer,
-		args: [sharedNFTLogicAddress],
-		log: true,
-	});
+  if (!zorbNFT) {
+    await deploy("ZorbNFT", {
+      from: deployer,
+      args: [sharedNFTLogicAddress],
+      log: true,
+    });
+  }
 };
 module.exports.tags = ["ZorbNFT"];
 module.exports.dependencies = ["TestSharedNFTLogic"];
