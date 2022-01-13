f8bd49a48fad0ae81f90c56782b960ecc55eeff7
Iain <iain@zora.co>
2022-01-13 15:09:58 -0500

    update solidity version

--- packages/zorb-contracts/contracts/test/TestSharedNFTLogic.sol
+++ packages/zorb-contracts/contracts/test/TestSharedNFTLogic.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.9;
+pragma solidity 0.8.10;
 
 import {SharedNFTLogic} from "@zoralabs/nft-editions-contracts/contracts/SharedNFTLogic.sol";
 
